9f38d7f8092a15e3304af426c4fe1d6c5b7ab7b0
szx <szx961102@gmail.com>
2019-04-27 16:44:06 +0800

    盛最多水的容器

---
+++ java/T11.java
@@ -0,0 +1,21 @@
+/**
+ * 11. 盛最多水的容器
+ * 
+ * 给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i,
+ * ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ */
+
+class Solution {
+  public int maxArea(int[] height) {
+    int max = 0, len = height.length, i = 0, j = len - 1;
+    while (i < j) {
+      max = Math.max(max, Math.min(height[i], height[j]) * (j - i));
+      if (height[i] < height[j]) {
+        i++;
+      } else {
+        j--;
+      }
+    }
+    return max;
+  }
+}
