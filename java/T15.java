c900904f40d274a250cff26f4add6a1ab2bb27c4
Jesse <jesse@cncarecc.com>
2019-04-30 14:10:04 +0800

    3sum

---
+++ java/T15.java
@@ -0,0 +1,40 @@
+/**
+三数之和
+给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+
+注意：答案中不可以包含重复的三元组。
+
+例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+
+满足要求的三元组集合为：
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+] */
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> list =new ArrayList<>();
+        int len = nums.length;
+        for(int i=0;i<len-2;i++){
+            if(i>0&&  nums[i]==nums[i-1]   ) continue;
+            int j=i+1,k=len-1;
+            while(j<k){
+                int tmp=nums[i]+nums[j]+nums[k];
+                if(tmp==0){
+                    list.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    while(j<k && nums[j]==nums[j+1]) j++;
+                    while(k>j && nums[k]==nums[k-1]) k--;
+                    j++;
+                    k--;
+                }else if(tmp>0){
+                    k--;
+                }else{
+                    j++;
+                }
+            }
+        }
+        return list;
+    }
+}
---
+++ java/T16.java
@@ -0,0 +1,28 @@
+/**
+最接近的三数之和
+给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+
+例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
+
+与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
+ */
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int len=nums.length;
+        Arrays.sort(nums);
+        int sum=nums[0]+nums[1]+nums[2];
+        for(int i=0;i<len-2;i++){
+            int j=i+1,k=len-1;
+            while(j<k){
+                int tmp=nums[i]+nums[j]+nums[k];
+                sum=Math.abs(tmp-target)<Math.abs(sum-target)?tmp:sum;
+                if(tmp==target){
+                    return tmp; 
+                }else if(tmp>target) k--;
+                else j++;
+            }
+        }
+        return sum;
+    }
+}
