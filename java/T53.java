cc4faa37390c19102716d225aff6a7484960607a
szx <szx961102@gmail.com>
2019-05-07 22:44:26 +0800

    最大子序和

--- java/T53.java
+++ java/T53.java
@@ -9,35 +9,18 @@
 
 // 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
 
-// class T53 {
-//   public int maxSubArray(int[] nums) {
-//     int n = nums.length;
-//     int sum = 0;
-//     int ret = nums[0];
-//     for (int i = 0; i < n; i++) {
-//         sum=sum>0?sum+nums[i]:nums[i];
-//         ret=ret<sum?sum:ret;
-//     }
-//     return ret;
-//   }
-//   public static void main(String[] args) {
-    
-//   }
-// }
-//分治法
-class T53{
+class T53 {
   public int maxSubArray(int[] nums) {
-    return this.solve(nums, 0, nums.length-1);
-  }
-  public int solve(int[] nums,int left,int right){
-    if(left>right) return 0;
-    if (right-left<2) {
-      return Math.max(nums[left], nums[right]);
+    int n = nums.length;
+    int sum = 0;
+    int ret = nums[0];
+    for (int i = 0; i < n; i++) {
+        sum=sum>0?sum+nums[i]:nums[i];
+        ret=ret<sum?sum:ret;
     }
-    int mid=(left+right)/2;
-    int leftMax=solve(nums, left, mid-1);
-    int rightMax=solve(nums, mid+1, right);
-    return Math.max(nums[mid], Math.max(leftMax, rightMax));
-
+    return ret;
+  }
+  public static void main(String[] args) {
+    
   }
 }
