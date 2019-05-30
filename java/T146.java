ad3a1277c9f68a84bc0249fcd2415b88c4597adf
Jesse <szx961102@gmail.com>
2019-05-31 00:40:32 +0800

    LRU缓存机制

--- java/T146.java
+++ java/T146.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 // 运用你所掌握的数据结构，设计和实现一个 LRU (最近最少使用) 缓存机制。它应该支持以下操作： 获取数据 get 和 写入数据 put 。
 
 // 获取数据 get(key) - 如果密钥 (key) 存在于缓存中，则获取密钥的值（总是正数），否则返回 -1。
@@ -23,18 +25,97 @@
 // cache.get(4); // 返回 4
 
 class LRUCache {
+  class ListNode {
+    public int key;
+    public int val;
+    public ListNode next;
+    public ListNode pre;
+
+    public ListNode(int key, int val) {
+      this.key = key;
+      this.val = val;
+    }
+  }
+
+  public ListNode head = new ListNode(0, 0), tail = head;
+  private Map<Integer, ListNode> map = new HashMap<>();
+  private int cap = 0;
 
   public LRUCache(int capacity) {
-
+    this.cap = capacity;
   }
 
   public int get(int key) {
+    if (this.map.containsKey(key)) {
+      ListNode node = this.map.get(key);
+      // 只有一个node
+      if (head.next == node) {
+        return node.val;
+      }
+      // node不是最后一个
+      if (node != tail) {
+        node.next.pre = node.pre;
+      } else {
+        tail = tail.pre;
+      }
+      node.pre.next = node.next;
+      head.next.pre = node;
+      node.next = head.next;
+      node.pre = head;
+      head.next = node;
 
+      return node.val;
+    }
+    return -1;
   }
 
   public void put(int key, int value) {
-
+    if (this.map.containsKey(key)) {
+      // 更新，不删除
+      this.map.computeIfPresent(key, (k, v) -> {
+        v.val = value;
+        return v;
+      });
+      // 放到head后
+      this.get(key);
+      return;
+    }
+    if (this.map.size() >= this.cap) {
+      // 删除最后的key
+      this.map.remove(this.tail.key);
+      this.tail = this.tail.pre;
+    }
+    ListNode node = new ListNode(key, value);
+    this.map.put(key, node);
+    if (this.tail == this.head) {
+      // 为空，直接插入
+      this.head.next = node;
+      node.pre = this.head;
+      tail = tail.next;
+      return;
+    }
+    // 不为空，插到head后
+    node.next = this.head.next;
+    node.pre = this.head;
+    this.head.next.pre = node;
+    this.head.next = node;
   }
+}
+
+public class T146 {
+  public static void main(String[] args) {
+    LRUCache a = new LRUCache(2);
+    System.out.println(a.get(2));
+    a.put(2, 6);
+    System.out.println(a.get(1));
+
+    a.put(1, 5);
+    a.put(1, 2);
+
+    System.out.println(a.get(1));
+    System.out.println(a.get(2));
+  }
+
 }
 
 /**
