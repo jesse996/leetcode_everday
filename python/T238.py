36076ab343eb85c4022bfd5fed433e0719196778
Jesse <szx961102@gmail.com>
2019-06-25 15:27:50 +0800

    a

--- python/T238.py
+++ python/T238.py
@@ -1,3 +1,21 @@
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        return [x for x in nums ]
+    def productExceptSelf(self, nums):
+        left = 1
+        right = 1
+        i = 0
+        output = list.copy(nums)
+        while i < len(nums):
+            output[i] = left
+            left *= nums[i]
+            i += 1
+        j = len(nums)-1
+        while j >= 0:
+            output[j] *= right
+            right *= nums[j]
+            j -= 1
+        return output
+
+
+s = Solution()
+nums = list(range(1, 5))
+print(s.productExceptSelf(nums))
