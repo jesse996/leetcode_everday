5f1858b46e45e58188181aaf61e67ef6343dcbb2
Jesse <szx961102@gmail.com>
2019-07-26 13:27:15 +0800

    删除链表的倒数第N个节点

--- java/T19.java
+++ java/T19.java
@@ -16,6 +16,7 @@ import java.util.Map;
  * Definition for singly-linked list. public class ListNode { int val; ListNode
  * next; ListNode(int x) { val = x; } }
  */
+// 一，用map
 class Solution {
   public ListNode removeNthFromEnd(ListNode head, int n) {
     int count = 0;// 总数
@@ -32,5 +33,22 @@ class Solution {
     ListNode pre = map.get(count - n - 1);
     pre.next = pre.next.next;
     return head;
+  }
+}
+
+// 2.双指针法
+class Solution {
+  public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode h = new ListNode(0);
+    h.next = head;
+    ListNode l1 = h, l2 = h;
+    for (int i = 0; i <= n; i++)
+      l1 = l1.next;
+    while (l1 != null) {
+      l1 = l1.next;
+      l2 = l2.next;
+    }
+    l2.next = l2.next.next;
+    return h.next;
   }
 }
