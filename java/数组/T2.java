de97b3816ea095d0e6f376d6a505e569e3fd64a8
Jesse <szx961102@gmail.com>
2020-08-04 20:27:49 +0800

    井字游戏

---
+++ java/数组/T2.java
@@ -0,0 +1,67 @@
+package 数组;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 设计一个算法，判断玩家是否赢了井字游戏。输入是一个 N x N 的数组棋盘，由字符" "，"X"和"O"组成，其中字符" "代表一个空位。
+ * <p>
+ * 以下是井字游戏的规则：
+ * <p>
+ * 玩家轮流将字符放入空位（" "）中。
+ * 第一个玩家总是放字符"O"，且第二个玩家总是放字符"X"。
+ * "X"和"O"只允许放置在空位中，不允许对已放有字符的位置进行填充。
+ * 当有N个相同（且非空）的字符填充任何行、列或对角线时，游戏结束，对应该字符的玩家获胜。
+ * 当所有位置非空时，也算为游戏结束。
+ * 如果游戏结束，玩家不允许再放置字符。
+ * 如果游戏存在获胜者，就返回该游戏的获胜者使用的字符（"X"或"O"）；如果游戏以平局结束，则返回 "Draw"；如果仍会有行动（游戏未结束），则返回 "Pending"。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入： board = ["O X"," XO","X O"]
+ * 输出： "X"
+ * 示例 2：
+ * <p>
+ * 输入： board = ["OOX","XXO","OXO"]
+ * 输出： "Draw"
+ * 解释： 没有玩家获胜且不存在空位
+ * 示例 3：
+ * <p>
+ * 输入： board = ["OOX","XXO","OX "]
+ * 输出： "Pending"
+ * 解释： 没有玩家获胜且仍存在空位
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/tic-tac-toe-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class T2 {
+    public String tictactoe(String[] board) {
+        int n = board.length;
+        int[] row = new int[n];
+        int[] col = new int[n];
+        int dia1 = 0, dia2 = 0;
+        boolean isFull = true;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int offset = 0;
+                if (board[i].charAt(j) == 'X') offset = 1;
+                else if (board[i].charAt(j) == 'O') offset = -1;
+                else isFull = false;
+                row[i] += offset;
+                col[j] += offset;
+                if (i == j) dia1 += offset;
+                if (i + j == n - 1) dia2 += offset;
+            }
+        }
+        if (dia1 == n || dia2 == n) return "X";
+        if (dia1 == -n || dia2 == -n) return "O";
+        for (int k = 0; k < n; k++) {
+            if (row[k] == n || col[k] == n) return "X";
+            else if (row[k] == -n || col[k] == -n) return "O";
+        }
+        return isFull?"Draw":"Pending";
+    }
+}
