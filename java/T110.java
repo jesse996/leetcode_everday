c6e879f99b702a44e5a54f685f52f392a78cdf25
Jesse <szx961102@gmail.com>
2020-09-18 09:49:40 +0800

    判断它是否是高度平衡的二叉树

---
+++ java/T110.java
@@ -0,0 +1,29 @@
+/**
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树
+ * <p>
+ * 一棵高度平衡二叉树定义为：
+ * <p>
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+ */
+
+public class T110 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return isBalanced(root.left) && isBalanced(root.right) && Math.abs(height(root.left) - height(root.right)) <= 1;
+    }
+
+    private int height(TreeNode node) {
+        if (node==null) return 0;
+        return Math.max(height(node.left),height(node.right))+1;
+    }
+}
