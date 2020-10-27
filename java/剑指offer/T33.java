83db5c905557b6095ded0046734227e689e2aa2c
jesse <szx961102@gmail.com>
2020-10-27 21:10:55 +0800

    二叉搜索树的后序遍历序列

---
+++ java/剑指offer/T33.java
@@ -0,0 +1,41 @@
+package 剑指offer;
+
+/**
+ * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果。如果是则返回 true，否则返回 false。
+ * 假设输入的数组的任意两个数字都互不相同
+ * 参考以下这颗二叉搜索树：
+ * <p>
+ * 5
+ * / \
+ * 2   6
+ * / \
+ * 1   3
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: [1,6,3,2,5]
+ * 输出: false
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入: [1,3,2,6,5]
+ * 输出: true
+ * <p>
+ * Created by jesse on 2020/10/27 下午8:58
+ */
+
+public class T33 {
+    public boolean verifyPostorder(int[] postorder) {
+        return recur(postorder, 0, postorder.length - 1);
+    }
+
+    public boolean recur(int[] postorder, int i, int j) {
+        if (i >= j) return true;
+        int p = i;
+        while (postorder[p] < postorder[j]) p++;
+        int m = p;
+        while (postorder[p] > postorder[j]) p++;
+        return p == j && recur(postorder, i, m - 1) && recur(postorder, m, j - 1);
+    }
+
+}
