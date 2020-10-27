eb2e4201e974d83f251ff4eddd6ecf0d6cd28d16
jesse <szx961102@gmail.com>
2020-10-27 21:26:43 +0800

    二叉搜索树的后序遍历序列

--- java/剑指offer/T33.java
+++ java/剑指offer/T33.java
@@ -1,11 +1,13 @@
 package 剑指offer;
 
+import java.util.Stack;
+
 /**
  * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果。如果是则返回 true，否则返回 false。
  * 假设输入的数组的任意两个数字都互不相同
  * 参考以下这颗二叉搜索树：
  * <p>
- * 5
+ *  5
  * / \
  * 2   6
  * / \
@@ -36,6 +38,19 @@ public class T33 {
         int m = p;
         while (postorder[p] > postorder[j]) p++;
         return p == j && recur(postorder, i, m - 1) && recur(postorder, m, j - 1);
+    }
+
+    //法二：单调栈
+    public boolean verifyPostorder2(int[] postorder) {
+        Stack<Integer> stack = new Stack<>();
+        int root = Integer.MAX_VALUE;
+        for (int i = postorder.length - 1; i >= 0; i--) {
+            if (postorder[i] > root) return false;
+            while (!stack.isEmpty() && stack.peek() > postorder[i])
+                root = stack.pop();
+            stack.add(postorder[i]);
+        }
+        return true;
     }
 
 }
