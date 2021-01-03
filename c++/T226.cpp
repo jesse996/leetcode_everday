15d1ecdefa089047482ba53b49f88175f21e7d85
Jesse <szx961102@gmail.com>
2021-01-03 14:41:27 +0800

    翻转二叉树

---
+++ c++/T226.cpp
@@ -0,0 +1,27 @@
+#include <MacTypes.h>
+
+//
+// Created by jesse on 2021/1/3.
+//
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode *invertTree(TreeNode *root) {
+        if (!root) {
+            return root;
+        }
+        auto tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
