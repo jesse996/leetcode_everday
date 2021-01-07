84ea14f1f23d804541bf861c0d13c54bd9549344
Jesse <szx961102@gmail.com>
2021-01-07 19:42:54 +0800

    下一个更大元素 II

--- java/T146.java
+++ java/T146.java
@@ -25,96 +25,122 @@ import java.util.*;
 // cache.get(4); // 返回 4
 
 class LRUCache {
-  class ListNode {
-    public int key;
-    public int val;
-    public ListNode next;
-    public ListNode pre;
+    class ListNode {
+        public int key;
+        public int val;
+        public ListNode next;
+        public ListNode pre;
 
-    public ListNode(int key, int val) {
-      this.key = key;
-      this.val = val;
+        public ListNode(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
     }
-  }
 
-  public ListNode head = new ListNode(0, 0), tail = head;
-  private Map<Integer, ListNode> map = new HashMap<>();
-  private int cap = 0;
-
-  public LRUCache(int capacity) {
-    this.cap = capacity;
-  }
-
-  public int get(int key) {
-    if (this.map.containsKey(key)) {
-      ListNode node = this.map.get(key);
-      // 只有一个node
-      if (head.next == node) {
-        return node.val;
-      }
-      // node不是最后一个
-      if (node != tail) {
-        node.next.pre = node.pre;
-      } else {
-        tail = tail.pre;
-      }
-      node.pre.next = node.next;
-      head.next.pre = node;
-      node.next = head.next;
-      node.pre = head;
-      head.next = node;
-
-      return node.val;
-    }
-    return -1;
-  }
-
-  public void put(int key, int value) {
-    if (this.map.containsKey(key)) {
-      // 更新，不删除
-      this.map.computeIfPresent(key, (k, v) -> {
-        v.val = value;
-        return v;
-      });
-      // 放到head后
-      this.get(key);
-      return;
-    }
-    if (this.map.size() >= this.cap) {
-      // 删除最后的key
-      this.map.remove(this.tail.key);
-      this.tail = this.tail.pre;
-    }
-    ListNode node = new ListNode(key, value);
-    this.map.put(key, node);
-    if (this.tail == this.head) {
-      // 为空，直接插入
-      this.head.next = node;
-      node.pre = this.head;
-      tail = tail.next;
-      return;
-    }
-    // 不为空，插到head后
-    node.next = this.head.next;
-    node.pre = this.head;
-    this.head.next.pre = node;
-    this.head.next = node;
-  }
+//  public ListNode head = new ListNode(0, 0), tail = head;
+//  private Map<Integer, ListNode> map = new HashMap<>();
+//  private int cap = 0;
+//
+//  public LRUCache(int capacity) {
+//    this.cap = capacity;
+//  }
+//
+//  public int get(int key) {
+//    if (this.map.containsKey(key)) {
+//      ListNode node = this.map.get(key);
+//      // 只有一个node
+//      if (head.next == node) {
+//        return node.val;
+//      }
+//      // node不是最后一个
+//      if (node != tail) {
+//        node.next.pre = node.pre;
+//      } else {
+//        tail = tail.pre;
+//      }
+//      node.pre.next = node.next;
+//      head.next.pre = node;
+//      node.next = head.next;
+//      node.pre = head;
+//      head.next = node;
+//
+//      return node.val;
+//    }
+//    return -1;
+//  }
+//
+//  public void put(int key, int value) {
+//    if (this.map.containsKey(key)) {
+//      // 更新，不删除
+//      this.map.computeIfPresent(key, (k, v) -> {
+//        v.val = value;
+//        return v;
+//      });
+//      // 放到head后
+//      this.get(key);
+//      return;
+//    }
+//    if (this.map.size() >= this.cap) {
+//      // 删除最后的key
+//      this.map.remove(this.tail.key);
+//      this.tail = this.tail.pre;
+//    }
+//    ListNode node = new ListNode(key, value);
+//    this.map.put(key, node);
+//    if (this.tail == this.head) {
+//      // 为空，直接插入
+//      this.head.next = node;
+//      node.pre = this.head;
+//      tail = tail.next;
+//      return;
+//    }
+//    // 不为空，插到head后
+//    node.next = this.head.next;
+//    node.pre = this.head;
+//    this.head.next.pre = node;
+//    this.head.next = node;
+//  }
 }
 
 public class T146 {
-  public static void main(String[] args) {
-    LRUCache a = new LRUCache(2);
-    System.out.println(a.get(2));
-    a.put(2, 6);
-    System.out.println(a.get(1));
+    class LRUCache {
+        private LinkedHashMap<Integer, Integer> map = new LinkedHashMap();
+        private int cap;
 
-    a.put(1, 5);
-    a.put(1, 2);
+        public LRUCache(int capacity) {
+            this.cap = capacity;
+        }
 
-    System.out.println(a.get(1));
-    System.out.println(a.get(2));
-  }
+        public int get(int key) {
+            if (map.containsKey(key)) {
+                makeRecent(key);
+                return map.get(key);
+            } else {
+                return -1;
+            }
+        }
+
+        public void put(int key, int value) {
+            if (map.containsKey(key)) {
+                map.put(key, value);
+                makeRecent(key);
+                return;
+            }
+            if (map.size() >= cap) {
+                int oldKey = map.entrySet().iterator().next().getKey();
+                map.remove(oldKey);
+            }
+            map.put(key, value);
+            cap++;
+        }
+
+        private void makeRecent(int key) {
+            int val = map.get(key);
+            map.remove(key);
+            map.put(key, val);
+        }
+    }
+
 
 }
 
