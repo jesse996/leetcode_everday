f7631f263ce30006e93c2b69fd1aa2cfea5d8af7
Jesse <szx961102@gmail.com>
2021-01-20 20:00:40 +0800

    二叉树的深度

---
+++ java/剑指offer/T55_1.java
@@ -0,0 +1,34 @@
+package 剑指offer;
+
+/**
+ * @Author: jesse
+ * @Date: 2021/1/20 7:59 下午
+ * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+ * <p>
+ * 例如：
+ * <p>
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ * <p>
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class T55_1 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root==null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
