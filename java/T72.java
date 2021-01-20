243401b4db9ab0a2ce6bfbbdc8e219fef1e7437f
Jesse <szx961102@gmail.com>
2021-01-20 17:51:46 +0800

    编辑距离

---
+++ java/T72.java
@@ -0,0 +1,38 @@
+/**
+ * @Author: jesse
+ * @Date: 2021/1/20 5:41 下午
+ * 给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
+ * <p>
+ * 你可以对一个单词进行如下三种操作：
+ * <p>
+ * 插入一个字符
+ * 删除一个字符
+ * 替换一个字符
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/edit-distance
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class T72 {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i][j - 1] + 1, Math.min(dp[i - 1][j] + 1, dp[i - 1][j - 1] + 1));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
