70482c30d763090cb50a94cde60df030f4661a44
Jesse <szx961102@gmail.com>
2021-01-23 13:58:40 +0800

    连通网络的操作次数

---
+++ java/T1319.java
@@ -0,0 +1,51 @@
+/**
+ * @Author: jesse
+ * @Date: 2021/1/23 1:17 下午
+ * 用以太网线缆将 n 台计算机连接成一个网络，计算机的编号从 0 到 n-1。线缆用 connections 表示，其中 connections[i] = [a, b] 连接了计算机 a 和 b。
+ * <p>
+ * 网络中的任何一台计算机都可以通过网络直接或者间接访问同一个网络中其他任意一台计算机。
+ * <p>
+ * 给你这个计算机网络的初始布线 connections，你可以拔开任意两台直连计算机之间的线缆，并用它连接一对未直连的计算机。请你计算并返回使所有计算机都连通所需的最少操作次数。如果不可能，则返回 -1 。 
+ * <p>
+ *  
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-of-operations-to-make-network-connected
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class T1319 {
+    int[] parent;
+
+    public int makeConnected(int n, int[][] connections) {
+        if (connections.length < n - 1) return -1;
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        for (int[] conn : connections) {
+            union(conn[0], conn[1]);
+        }
+        int count=0;
+        for (int i = 0; i < n; i++) {
+            if (i == parent[i]) {
+                count++;
+            }
+        }
+        return count - 1;
+    }
+
+    public void union(int a, int b) {
+        int pa=find(a);
+        int pb = find(b);
+        if (pa==pb) return;
+        parent[pb]=pa;
+    }
+
+    public int find(int a) {
+        while (parent[a] != a) {
+            parent[a] = parent[parent[a]];
+            a = parent[a];
+        }
+        return a;
+    }
+}
