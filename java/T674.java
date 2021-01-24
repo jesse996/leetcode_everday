8919d235ffdb4c62a4bcf530cbb2ed964021df6c
Jesse <szx961102@gmail.com>
2021-01-24 13:26:24 +0800

    最长连续递增序列

---
+++ java/T674.java
@@ -0,0 +1,27 @@
+/**
+ * @Author: jesse
+ * @Date: 2021/1/24 12:51 下午
+ * 给定一个未经排序的整数数组，找到最长且 连续递增的子序列，并返回该序列的长度。
+ * <p>
+ * 连续递增的子序列 可以由两个下标 l 和 r（l < r）确定，如果对于每个 l <= i < r，都有 nums[i] < nums[i + 1] ，那么子序列 [nums[l], nums[l + 1], ..., nums[r - 1], nums[r]] 就是连续递增子序列。
+ * <p>
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-continuous-increasing-subsequence
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class T674 {
+    public int findLengthOfLCIS(int[] nums) {
+        int ans = 0;
+        int n = nums.length;
+        int start = 0;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] <= nums[i - 1]) {
+                start = i;
+            }
+            ans = Math.max(ans, i - start + 1);
+        }
+        return ans;
+    }
+}
