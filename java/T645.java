890c3f79f4f60141dbb0a808d3c0b40f1bc7c995
Jesse <szx961102@gmail.com>
2021-01-31 11:27:02 +0800

    错误的集合

---
+++ java/T645.java
@@ -0,0 +1,35 @@
+/**
+ * @Author: jesse
+ * @Date: 2021/1/31 11:15 上午
+ * 集合 s 包含从 1 到 n 的整数。不幸的是，因为数据错误，导致集合里面某一个数字复制了成了集合里面的另外一个数字的值，导致集合 丢失了一个数字 并且 有一个数字重复 。
+ * <p>
+ * 给定一个数组 nums 代表了集合 S 发生错误后的结果。
+ * <p>
+ * 请你找出重复出现的整数，再找到丢失的整数，将它们以数组的形式返回。
+ * <p>
+ *  
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/set-mismatch
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class T645 {
+    public int[] findErrorNums(int[] nums) {
+        int[] res = new int[2];
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] *= -1;
+            } else {
+                res[0] = index + 1;
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            if (nums[i - 1] > 0) {
+                res[1] = i;
+            }
+        }
+        return res;
+    }
+}
