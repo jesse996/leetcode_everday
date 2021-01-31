35f46ef8bfd012353f6932b27b11293126dec2ab
Jesse <szx961102@gmail.com>
2021-01-31 10:15:27 +0800

    超级次方

--- java/T372.java
+++ java/T372.java
@@ -18,13 +18,15 @@ public class T372 {
         return (sub * rest) % base;
     }
 
+    //快速幂
     public int myPow(int a, int k) {
+        if (k == 1) return 1;
         a %= base;
-        int res = 1;
-        for (int i = 0; i < k; i++) {
-            res *= a;
-            res %= base;
+        if (k % 2 == 1) {
+            return (a * myPow(a, k - 1)) % base;
+        } else {
+            int sub = myPow(a, k / 2);
+            return (sub * sub) % base;
         }
-        return res;
     }
 }
