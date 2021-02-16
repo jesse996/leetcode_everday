afc2e17bcc314633719638b6eab44e6d81c4b52c
jesse <szx961102@gmail.com>
2021-02-16 10:21:33 +0800

    数组拆分 I

---
+++ java/T561.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+/**
+ * Created by jesse on 2021/2/16 上午10:16
+ * 给定长度为 2n 的整数数组 nums ，你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从 1 到 n 的 min(ai, bi) 总和最大。
+ *
+ * 返回该 最大总和 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/array-partition-i
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class T561 {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int res = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            res += nums[i];
+        }
+        return res;
+    }
+}
