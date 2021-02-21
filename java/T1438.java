14a1314d6e8e71c243c85c88a7fae38e2ef9847a
Jesse <szx961102@gmail.com>
2021-02-21 14:46:31 +0800

    绝对差不超过限制的最长连续子数组

--- java/T1438.java
+++ java/T1438.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.TreeMap;
 
 /**
@@ -14,22 +15,52 @@ import java.util.TreeMap;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class T1438 {
+//    public int longestSubarray(int[] nums, int limit) {
+//        int n = nums.length;
+//        int left = 0, right = 0;
+//        int res = 0;
+//        TreeMap<Integer, Integer> map = new TreeMap<>();
+//        while (right < n) {
+//            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+//            right++;
+//            while (map.lastKey() - map.firstKey() > limit) {
+//                map.put(nums[left], map.get(nums[left]) - 1);
+//                if (map.get(nums[left]) == 0) {
+//                    map.remove(nums[left]);
+//                }
+//                left++;
+//            }
+//            res = Math.max(res, right - left);
+//        }
+//        return res;
+//    }
+
+    //单调队列
     public int longestSubarray(int[] nums, int limit) {
-        int n = nums.length;
+        LinkedList<Integer> minQueue = new LinkedList<>(), maxQueue = new LinkedList<>();
         int left = 0, right = 0;
-        int res = 0;
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int n = nums.length;
+        int res=0;
         while (right < n) {
-            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
-            right++;
-            while (map.lastKey() - map.firstKey() > limit) {
-                map.put(nums[left], map.get(nums[left]) - 1);
-                if (map.get(nums[left]) == 0) {
-                    map.remove(nums[left]);
+            while (!minQueue.isEmpty() && minQueue.getLast() > nums[right]) {
+                minQueue.pollLast();
+            }
+            while (!maxQueue.isEmpty() && maxQueue.getLast() < nums[right]) {
+                maxQueue.pollLast();
+            }
+            minQueue.addLast(nums[right]);
+            maxQueue.addLast(nums[right]);
+            while (maxQueue.getFirst() - minQueue.getFirst() > limit) {
+                if (minQueue.getFirst() == nums[left]) {
+                    minQueue.pollFirst();
+                }
+                if (maxQueue.getFirst() == nums[left]) {
+                    maxQueue.pollFirst();
                 }
                 left++;
             }
-            res = Math.max(res, right - left);
+            right++;
+            res = Math.max(res,right - left);
         }
         return res;
     }
