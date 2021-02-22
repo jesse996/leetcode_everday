78cac9fdb0d845a907b9da69f8734a3712056f74
Jesse <szx961102@gmail.com>
2021-02-22 14:34:08 +0800

    托普利茨矩阵

---
+++ java/T766.java
@@ -0,0 +1,25 @@
+/**
+ * @Author: jesse
+ * @Date: 2021/2/22 2:26 下午
+ * 给你一个 m x n 的矩阵 matrix 。如果这个矩阵是托普利茨矩阵，返回 true ；否则，返回 false 。
+ * <p>
+ * 如果矩阵上每一条由左上到右下的对角线上的元素都相同，那么这个矩阵是 托普利茨矩阵 。
+ * <p>
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/toeplitz-matrix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class T766 {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j]!=matrix[i-1][j-1]) return false;
+            }
+        }
+        return true;
+    }
+}
