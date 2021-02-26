eea8c4dea23d47f82baf11c01640ad70e4e52440
Jesse <szx961102@gmail.com>
2021-02-26 18:00:56 +0800

    猜字谜

---
+++ java/T1178.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+/**
+ * @Author: jesse
+ * @Date: 2021/2/26 11:41 上午
+ * 外国友人仿照中国字谜设计了一个英文版猜字谜小游戏，请你来猜猜看吧。
+ * <p>
+ * 字谜的迷面 puzzle 按字符串形式给出，如果一个单词 word 符合下面两个条件，那么它就可以算作谜底：
+ * <p>
+ * 单词 word 中包含谜面 puzzle 的第一个字母。
+ * 单词 word 中的每一个字母都可以在谜面 puzzle 中找到。
+ * 例如，如果字谜的谜面是 "abcdefg"，那么可以作为谜底的单词有 "faced", "cabbage", 和 "baggage"；而 "beefed"（不含字母 "a"）以及 "based"（其中的 "s" 没有出现在谜面中）。
+ * 返回一个答案数组 answer，数组中的每个元素 answer[i] 是在给出的单词列表 words 中可以作为字谜迷面 puzzles[i] 所对应的谜底的单词数目。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-of-valid-words-for-each-puzzle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class T1178 {
+    public List<Integer> findNumOfValidWords(String[] words, String[] puzzles) {
+        Map<Integer, Integer> frequency = new HashMap<Integer, Integer>();
+
+        for (String word : words) {
+            int mask = 0;
+            for (int i = 0; i < word.length(); ++i) {
+                char ch = word.charAt(i);
+                mask |= (1 << (ch - 'a'));
+            }
+            if (Integer.bitCount(mask) <= 7) {
+                frequency.put(mask, frequency.getOrDefault(mask, 0) + 1);
+            }
+        }
+
+        List<Integer> ans = new ArrayList<Integer>();
+        for (String puzzle : puzzles) {
+            int total = 0;
+
+            // 枚举子集方法一
+            // for (int choose = 0; choose < (1 << 6); ++choose) {
+            //     int mask = 0;
+            //     for (int i = 0; i < 6; ++i) {
+            //         if ((choose & (1 << i)) != 0) {
+            //             mask |= (1 << (puzzle.charAt(i + 1) - 'a'));
+            //         }
+            //     }
+            //     mask |= (1 << (puzzle.charAt(0) - 'a'));
+            //     if (frequency.containsKey(mask)) {
+            //         total += frequency.get(mask);
+            //     }
+            // }
+
+            // 枚举子集方法二
+            int mask = 0;
+            for (int i = 1; i < 7; ++i) {
+                mask |= (1 << (puzzle.charAt(i) - 'a'));
+            }
+            int subset = mask;
+            do {
+                int s = subset | (1 << (puzzle.charAt(0) - 'a'));
+                if (frequency.containsKey(s)) {
+                    total += frequency.get(s);
+                }
+                subset = (subset - 1) & mask;
+            } while (subset != mask);
+            //循环终止条件是while(subset != mask)而不是while(subset !=0) 是因为subset是不含最高位的
+            // 即使是subset = 0 还需要执行一次循环(即子集只含首字母的情况) 1 0 0 0 0 0 0 这个时候(subset - 1) & mask就是mask本身了 终止循环
+
+            ans.add(total);
+        }
+        return ans;
+    }
+
+
+}
